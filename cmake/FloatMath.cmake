28c529a347b76cce03426304bec28f6937eecd08
jaanita <61026966+jaanita@users.noreply.github.com>
2022-04-22 18:07:26 -0500

    delete everything that is not energy-scan

--- cmake/FloatMath.cmake
+++
@@ -1,7 +0,0 @@
-function(float_math VAR expr)
-    execute_process(COMMAND "python3" "-Sc" "print(${expr})"
-                    OUTPUT_VARIABLE output
-                    OUTPUT_STRIP_TRAILING_WHITESPACE)
-    set(${VAR} ${output} PARENT_SCOPE)
-endfunction()
-
